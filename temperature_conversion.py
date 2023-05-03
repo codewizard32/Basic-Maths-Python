e10d8bf44ff79ee3a001356cd02523e8d16822ee
codewizard32 <131467398+codewizard32@users.noreply.github.com>
2023-05-03 12:15:04 +0530

    Update temperature_conversion.py

--- temperature_conversion.py
+++ temperature_conversion.py
@@ -1,3 +1,8 @@
 f = int(input("Enter temperature in Fahrenheit: "))
 c = (f - 32) * 5 / 9
 print("Temperature in Celsius is:", c)
+
+'''
+Enter temperature in Fahrenheit: 98
+Temperature in Celsius is: 36.666666666666664
+'''
